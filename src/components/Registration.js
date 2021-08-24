f55b14c9210d4ba40973ecdbf781eb3ad6670fc8
dalvine <dalvine@ya.ru>
2021-08-24 18:44:50 +0300

    fix: исправление ввода данных при регистрации

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -6,7 +6,7 @@ import auth from '../utils/auth'
 function Registration() {
 
     const history = useHistory()
-    
+
     const [userData, setUserData] = React.useState({})
     const [statusReg, setStatusReg] = React.useState(false)
     const [openModalResultRegister, setOpenModalResultRegister] = React.useState(false)
@@ -41,8 +41,8 @@ function Registration() {
         <>
             <form className="form form_place_auth" onSubmit={handleSubmit}>
                 <h2 className="form__header">Регистрация</h2>
-                <input type="email" className="form__input form__input_place_auth" placeholder="Email" name="email" value="" onChange={handleChange} required />
-                <input type="password" className="form__input form__input_place_auth" placeholder="Пароль" name="password" value="" onChange={handleChange} required />
+                <input type="email" className="form__input form__input_place_auth" placeholder="Email" name="email" value={userData.email} onChange={handleChange} required />
+                <input type="password" className="form__input form__input_place_auth" placeholder="Пароль" name="password" value={userData.password} onChange={handleChange} required />
                 <button type="submit" className="form__button form__button_place_auth">Зарегистрироваться</button>
                 <p className="form__text">Уже зарегистрированы? <Link to="/sign-in" className="form__link">Войти</Link></p>
             </form>
